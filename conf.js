dfcd9bfb327a797f80b7019185946a49b77478a4
Wolke <wolkesau@gmail.com>
2018-02-07 01:17:41 +0800

    begin

---
+++ conf.js
@@ -0,0 +1,12 @@
+{
+    DatabaseName: "admin",
+    collectionName: "botState",
+    mongoIp: "10.0.31.18",
+    mongoPort: "27017",
+    username: "adminadmin",
+    password: "adminadmin",
+    channelId: "1487202031",
+    channelSecret: "64078989ba8249519163b052eca6bc58",
+    channelAccessToken:  "QELaTKb+JpKNt+LndfixVD8EA+DGID5wgvZ10skM3F2nPPzvTC7ZpvxQ3onkR+hu06eRv1S+NG6Cfyw3EtfW21K6x6RGBRqf8ehPYUalja77myU10cSBR9GmYA/HDri9jDg5YqDHUVg5JCrkb+nnygdB04t89/1O/w1cDnyilFU="
+  
+}
---
+++ handler.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var config = require("./config")
+var botbuilder_linebot_connector_1 = require("botbuilder-linebot-connector");
+var botbuilder_mongodb_storage_1 = require("botbuilder-mongodb-storage");
+var builder = require('botbuilder');
+var connector = new botbuilder_linebot_connector_1.LineConnector({
+  hasPushApi: false,
+  // your line
+  channelId: process.env.channelId || config.channelId,
+  channelSecret: process.env.channelSecret || config.channelSecret,
+  channelAccessToken: process.env.channelAccessToken || config.channelAccessToken
+});
+
+var bot = new builder.UniversalBot(connector)
+  .set("storage", new botbuilder_mongodb_storage_1.MongoDbStorage({
+    DatabaseName: config.DatabaseName,
+    collectionName: config.collectionName,
+    mongoIp: config.mongoIp,
+    mongoPort: config.mongoPort,
+    // mongoIp: "ds125578.mlab.com",
+    // mongoPort: "255xx",
+    username: config.username,
+    password: config.password
+  }));
+  bot.dialog("/", [
+    function (s) { builder.Prompts.text(s, "name?"); },
+    function (s, r) {
+        s.userData.name = r.response;
+        console.log("after name", s.userData);
+        builder.Prompts.number(s, "age?");
+    },
+    function (s, r) {
+        console.log("after age", s.userData);
+        s.userData.age = r.response;
+        s.endDialog("bady " + s.userData.name);
+    }
+]);
+module.exports.line = (event, context, callback) => {
+  connector.serverlessWebhock(event)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
