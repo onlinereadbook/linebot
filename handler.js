ba9caef90b27c86305e8da0491f777f3c13f3c34
Wolke <wolkesau@gmail.com>
2018-03-17 14:54:03 +0800

    ok

--- handler.js
+++ handler.js
@@ -1,11 +1,41 @@
 'use strict';
+// Import dependencies
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const { DynamoBotStorage } = require('botbuilder-storage');
+
+// Instantiate the bot with a connector instance
+// const bot = new UniversalBot(connector);
+
+// Create a DynamoDB client and select the AWS 
+// region that hosts your instance of DynamoDB.
+const client = new DynamoDB({ region: "ap-northeast-1" });
+
+// Define the adapter settings
+const settings = {
+  // Required
+  tableName: process.env.DYNAMODB_TABLE,
+
+  // Required
+  primaryKey: "id",
+
+  // Optional but strongly recommended!
+  ttl: {
+    userData: 3600 * 24 * 365, // a year,
+    conversationData: 3600 * 24 * 7, // a week,
+    privateConversationData: 3600 * 24 * 7
+  }
+};
+
+// Instantiate the adapter with the client and settings.
+const adapter = new DynamoBotStorage(client, settings)
+
 
 var config = require("./conf");
-console.log("config",config)
+console.log("config", config)
 
 var bot_dailog = require("./bot/bot")
 var botbuilder_linebot_connector_1 = require("botbuilder-linebot-connector");
-var botbuilder_mongodb_storage_1 = require("botbuilder-mongodb-storage");
+// var botbuilder_mongodb_storage_1 = require("botbuilder-mongodb-storage");
 var builder = require('botbuilder');
 var connector = new botbuilder_linebot_connector_1.LineConnector({
   hasPushApi: false,
@@ -15,16 +45,16 @@ var connector = new botbuilder_linebot_connector_1.LineConnector({
   channelAccessToken: process.env.channelAccessToken || config.channelAccessToken
 });
 
-var bot = new builder.UniversalBot(connector)
-  .set("storage", new botbuilder_mongodb_storage_1.MongoDbStorage({
-    DatabaseName: config.DatabaseName,
-    collectionName: config.collectionName,
-    mongoIp: config.mongoIp,
-    mongoPort: config.mongoPort,
-    username: config.username,
-    password: config.password
-  }));
-  bot_dailog.default(bot)
+var bot = new builder.UniversalBot(connector).set('storage', adapter);
+// .set("storage", new botbuilder_mongodb_storage_1.MongoDbStorage({
+//   DatabaseName: config.DatabaseName,
+//   collectionName: config.collectionName,
+//   mongoIp: config.mongoIp,
+//   mongoPort: config.mongoPort,
+//   username: config.username,
+//   password: config.password
+// }));
+bot_dailog.default(bot)
 
 module.exports.line = (event, context, callback) => {
   connector.serverlessWebhock(event)
